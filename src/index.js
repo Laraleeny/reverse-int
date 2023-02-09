1ec90d37468f9fd8110342e517f746859f7b99f1
Laraleeny <lkozlova1428@gmail.com>
2023-02-09 13:35:03 +0200

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let result = '';
+      if(str[0] === '-') {
+        str = str.slice(1);
+      }
+    for (let i = 0; i < str.length; i++) {
+     result = `${str[i]}${result}`;
+    }
+  return Number(result);
 }
